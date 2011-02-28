409a7f3632485ed8ec1fbcf84bad60a1eeb942d3
Denys P <dector9@gmail.com>
2011-02-28 21:25:33 +0200

    Codestyle fixed

--- mpd_state.lua
+++ mpd_state.lua
@@ -1,24 +1,29 @@
-    mpd_widget = widget({ type = "textbox", name = "tb_mpd",
-                             align = "right" })
+--[[
+    Music Player Daemon (MPD) Widget State for Awesome WM
+    Developed by dector, Feb 2011
+    Contributed on GPL3 Licence
+--]]
 
-    function update_mpd_state(widget)
-        local fd = io.popen("mpc status -f 'Title: %title%\nArtist: %artist%'")
-        local status = fd:read("*all")
-        fd:close()
+mpd_widget = widget({ type = "textbox", name = "tb_mpd", align = "right" })
 
-	state = string.match(status, "%[(%w+)%]")
-	artist = string.match(status, "Artist: ([%w\ \"']+)")
-	title = string.match(status, "Title: ([%w\ \"']+)")
+function update_mpd_state(widget)
+    local fd = io.popen("mpc status -f 'Title: %title%\nArtist: %artist%'")
+    local status = fd:read("*all")
+    fd:close()
 
-	if state == nil then
-	    state_str = "<b>MPD</b>: Stopped"
-	elseif state == "paused" then
-	    state_str = "<b>MPD</b>: Paused"
-	else
-	    state_str = artist .. " - " .. title
-	end
-	widget.text = state_str
+    local state = string.match(status, "%[(%w+)%]")
+    local artist = string.match(status, "Artist: ([%w\ \"']+)")
+    local title = string.match(status, "Title: ([%w\ \"']+)")
+
+    if state == nil then
+        local state_str = "<b>MPD</b>: Stopped"
+    elseif state == "paused" then
+        local state_str = "<b>MPD</b>: Paused"
+    else
+        local state_str = artist .. " - " .. title
     end
+        widget.text = state_str
+end
 
     update_mpd_state(mpd_widget)
     awful.hooks.timer.register(1, function () update_mpd_state(mpd_widget) end)
